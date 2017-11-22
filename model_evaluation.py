9434932370f398f5f984345c4603d87a2f88f6cb
Manva Pradhan <pradhanmanva@gmail.com>
2017-11-22 02:38:39 -0600

    semi-final commit, optimization pending

--- model_evaluation.py
+++ model_evaluation.py
@@ -1,26 +1,67 @@
-'''
-numFolds = k #(where k >= 10)
-classifiers = {c1, c2, …, cn} #list of n classifiers with best parameters
-split the data into k folds d[1…k]
-for i in 1 to numFolds
-    # create training dataset by combining all folds except d[i]
-    train = {d[1] + d[2] + … + d[i-1] + d[i+1] + … + d[k]}
-    # create test dataset using d[i]
-    test = d[i]
-    for c in classifiers classifiers
-        # create a model of type c using train
-        model <- createModel(c, train)
-        # find accuracy of model of type c on test
-        for classifier c: accuracy[i] <- findAccuracy(model, test)
-        for classifier c: other_parameter[i] <- findEvaluation(model, test)
-At the end of the code, you will output the average of the accuracy and
-other evaluation parameter for each classifier i.e.
-average accuracy = average (accuracy[1], accuracy[2], …, accuracy[n])
-average other_parameter = average (other_parameter[1], other_parameter[2], …,other_parameter [n])
-'''
+import sys
+import warnings
+
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.model_selection import KFold, cross_val_score, cross_val_predict
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+from preprocessing import *
+
+warnings.filterwarnings("ignore")
 
 
-if __name__ == "__main__":
+def get_classifiers():
+    classifiers = [
+        DecisionTreeClassifier(criterion="entropy"),
+        Perceptron(penalty='l1'),
+        MLPClassifier(solver='sgd', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1),
+        MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1),
+        svm.LinearSVC(),
+        GaussianNB(),
+        LogisticRegression(random_state=0),
+        KNeighborsClassifier(),
+        BaggingClassifier(),
+        RandomForestClassifier(max_depth=10, random_state=0),
+        AdaBoostClassifier(random_state=0),
+        GradientBoostingClassifier()
+    ]
+    return classifiers
 
-    num_folds = 5
-    classifiers = get_classifiers()
+
+if __name__ == '__main__':
+    train_path, test_path = sys.argv[1:3]
+    train_data, test_data = get_preprocessed_data(train_path, test_path)
+    num_folds = sys.argv[3]
+    models = get_classifiers()
+    # ['dtc', 'perceptron', 'ann', 'dlp', 'svm', 'nb', 'logreg', 'knn', 'bagging', 'randomforest', 'adaboost','gradboost']
+    features = train_data.columns.values[:-1]
+    cls = train_data.columns.values[-1]
+    X = train_data[features].values
+    y = train_data[cls].values
+    print("yes")
+    kf = KFold(n_splits=2)
+    kf.get_n_splits(X)  # returns the number of splitting iterations in the cross-validation
+    print(kf)
+    for train_index, test_index in kf.split(X):
+        print('TRAIN:' + str(train_index) + 'TEST:' + str(test_index))
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        print(X_train)
+        print(X_test)
+        print(y_train)
+        print(y_test)
+        for c in models:
+            print(c)
+            classifier = c.fit(X_train, y_train)
+            scores = cross_val_score(c, X_test, y_test, cv=6)
+            print(scores)
+            predictions = cross_val_predict(c, X_train, y_train, cv=6)
+            print(predictions)
+            # print(accuracy_score(y, predictions))
+            # print(roc_curve(y, predictions))
+            # print(precision_recall_fscore_support(y, predictions))
--- preprocessing.py
+++ preprocessing.py
@@ -3,33 +3,10 @@ import pandas as pd
 import seaborn
 
 
-def check_data(df):
-    print(df.dtypes)
-    print(df.describe())
-    print(df.isnull().any())
-
-
 def print_plots(df):
-    df['s1'].plot.hist()
-    print(plt.show())
-    df['s2'].plot.hist()
-    print(plt.show())
-    df['s3'].plot.hist()
-    print(plt.show())
-    df['s4'].plot.hist()
-    print(plt.show())
-    df['s5'].plot.hist()
-    print(plt.show())
-    df['c1'].plot.hist()
-    print(plt.show())
-    df['c2'].plot.hist()
-    print(plt.show())
-    df['c3'].plot.hist()
-    print(plt.show())
-    df['c4'].plot.hist()
-    print(plt.show())
-    df['c5'].plot.hist()
-    print(plt.show())
+    for attr in list(df.columns.values):
+        df[attr].plot.hist()
+        print(plt.show())
     seaborn.pairplot(df, hue='class')
     print(plt.show())
 
@@ -37,26 +14,25 @@ def print_plots(df):
 def import_data(training_data, test_data):
     df_train = pd.read_csv(training_data, header=None)
     df_test = pd.read_csv(test_data, header=None)
-    df_train.columns = ["s1", "c1", "s2", "c2", "s3", "c3", "s4", "c4", "s5", "c5", "class"]
-    df_test.columns = ["s1", "c1", "s2", "c2", "s3", "c3", "s4", "c4", "s5", "c5", "class"]
-    print(df_train.shape)
-    print(df_test.shape)
+    df_train.columns = ['s1', 'c1', 's2', 'c2', 's3', 'c3', 's4', 'c4', 's5', 'c5', 'class']
+    df_test.columns = ['s1', 'c1', 's2', 'c2', 's3', 'c3', 's4', 'c4', 's5', 'c5', 'class']
+    print('Unprocessed Training Shape: ' + str(df_train.shape))
+    print('Unprocessed Testing Shape: ' + str(df_test.shape))
     df_train = df_train.drop_duplicates()
     df_test = df_test.drop_duplicates()
     df_train = df_train.dropna()
     df_test = df_test.dropna()
-    print(df_train.shape)
-    print(df_test.shape)
+    print('Preprocessed Training Shape: ' + str(df_train.shape))
+    print('Preprocessed Testing Shape: ' + str(df_test.shape))
     return df_train, df_test
 
 
-def get_preprecessed_data(path_train, path_test):
+def get_preprocessed_data(path_train, path_test):
     train, test = import_data(path_train, path_test)
-    check_data(train)
-    check_data(test)
+    print(pd.DataFrame.transpose(train.describe()))
+    print(pd.DataFrame.transpose(test.describe()))
     # print_plots(train)
     # print_plots(test)
-    pd.DataFrame.to_csv(train, "clean_train.csv")
-    pd.DataFrame.to_csv(test, "clean_test.csv")
-
-get_preprecessed_data("")
+    print('Pre-processing done.')
+    return train,test
+# get_preprocessed_data('poker-train.csv', 'poker-test.csv', 'clean-train', 'clean-test')
